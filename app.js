ab018711c7dfae4f071b84c17f4b86ef08ee2065
Sanwuthree <three@sanwu.org>
2017-09-22 02:25:10 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const {app,electron} = require("electron")
 const GConfig=require("./config")
-
+const fs=require("fs"),path=require("path"),process=require("process")
 app.on('ready',(info)=>{
     //const indexView=require("./views/index");
-    require("./views/login")
-})
+    if(!fs.existsSync(path.join(process.cwd(),"config.cfg"))){
+        require("./views/login")
+    }else{
+        require("./views/register")
+    }
+    
+});
---
+++ views/register.js
@@ -0,0 +1,12 @@
+const {ipcMain,BrowserWindow}=require("electron")
+const crypto=require("crypto"),qiniu=require("qiniu");
+
+let win = new BrowserWindow({
+    width:500,
+    height:400,
+    frame:false
+})
+win.loadURL(`file://${__dirname}/register.html`);
+//win.webContents.openDevTools()
+
+module.exports=win;
